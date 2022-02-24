8c5b4444aebbb9591dee1a73106a700d40879131
Abdullah Abid <52856978+AbdullahAbid87@users.noreply.github.com>
2022-02-24 20:13:51 +0500

    Update Lottery.sol
    Added enter function

--- Lottery.sol
+++ Lottery.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Lottery{
-  // Data Types 
-  address public haris;
-  
-  constructor(){
-     haris = msg.sender;
-  }
+contract Lottery {
+    //  data types
+    address public manager;
+    address payable[] public players;
+    
+    constructor(){
+     manager = msg.sender;
+    }
+
+   function enter() public payable{
+        require(msg.value > 0.01 ether);
+        players.push(payable(msg.sender));
+    }
 }
